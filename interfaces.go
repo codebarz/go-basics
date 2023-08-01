8a75f2570d20ee715211a222ff17fc46d9c4c31e
codebarz <oketegah@gmail.com>
2023-08-01 12:19:29 +0100

    interfaces

--- interfaces.go
+++ interfaces.go
@@ -2,15 +2,39 @@ package main
 
 import "log"
 
+type Animal interface {
+	Say() string
+	NumberOfLegs() int
+}
+
 type Dog struct {
-	Name string
+	Breed string
+	Color string
 }
 
 func interfaces() {
-	dog := Dog{"Lizzy"}
-	dog.bark()
+
+	log.Println("=========== INTERFACES ===========")
+
+	dog := Dog{
+		Breed: "Rot",
+		Color: "black",
+	}
+
+	action(&dog)
+
+	log.Println("=========== INTERFACES ===========")
+
 }
 
-func (*Dog) bark() {
-	log.Println("Woof woof")
+func action(a Animal) {
+	log.Println("The animal says ", a.Say(), " and the animal has ", a.NumberOfLegs(), " legs")
+}
+
+func (d *Dog) Say() string {
+	return "Woof"
+}
+
+func (d *Dog) NumberOfLegs() int {
+	return 4
 }
