3b54c0c685fab02450151e6531db9c02d87a5a86
codebarz <oketegah@gmail.com>
2023-10-10 23:20:02 +0100

    feat: channels

---
+++ json.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type NewUser struct {
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	IsBlack bool   `json:"is_black"`
+}
+
+func Json() {
+	log.Println("=========== JSON ===========")
+	// read json
+	newJson := `
+	[
+		{
+			"name" : "Mike",
+			"age": 20,
+			"is_black": false
+		}
+	]
+	`
+
+	var unmarshaledJson []NewUser
+
+	err := json.Unmarshal([]byte(newJson), &unmarshaledJson)
+
+	if err != nil {
+		log.Println("An error occured: %v", err)
+	}
+
+	log.Println(unmarshaledJson)
+
+	log.Println("=========== JSON ===========")
+
+}
